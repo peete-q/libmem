7a8bbb1f8c155dceec097afc569671261eff8d39
peter-qy <peter_qy@sina.cn>
2013-05-08 12:30:50 +0800

    Update libmem.h

--- libmem.h
+++ libmem.h
@@ -1,34 +1,15 @@
 
-#include "stddef.h"
+#ifndef __LIBMEM_H__
+#define __LIBMEM_H__
 
-typedef struct {
-	size_t node;
-	char base[0];
-} libmem_Segment;
+#include <stddef.h>
 
-typedef struct {
-	size_t sibling	: 30;
-	size_t parent	: 30;
-	size_t prevous	: 30;
-	size_t next		: 30;
-	size_t level	: 7;
-	size_t free		: 1;
-	libmem_Segment* segment;
-} libmem_Node;
+struct libmem;
 
-typedef struct {
-	size_t minlevel;
-	size_t maxlevel;
-	size_t nodefree;
-	size_t nodesize;
-	char* base;
-	size_t* free;
-	libmem_Node* node;
-} libmem;
+struct libmem* libmem_new(size_t minlevel, size_t maxlevel);
+void libmem_delete(struct libmem* self);
+void* libmem_alloc(struct libmem* self, size_t size);
+void* libmem_realloc(struct libmem* self, void* ptr, size_t size);
+int libmem_free(struct libmem* self, void* ptr);
 
-
-libmem* libmem_new(size_t minlevel, size_t maxlevel);
-void libmem_delete(libmem* self);
-void* libmem_alloc(libmem* self, size_t size);
-void* libmem_realloc(libmem* self, void* ptr, size_t size);
-int libmem_free(libmem* self, void* ptr);
+#endif
