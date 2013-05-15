9a1d6169093c3e6f9caa28e9e2dc48243e2faa06
peteryan-q <peter_qy@sina.cn>
2013-05-15 20:23:48 +0800

    Update libmem.c

--- libmem.c
+++ libmem.c
@@ -4,7 +4,6 @@
 
 #define INVALID -1
 
-
 struct libmem_Segment {
 	size_t index;
 	char base[0];
@@ -30,7 +29,7 @@ struct libmem {
 	struct libmem_Node* node;
 };
 
-static void libmem_resizenode(struct libmem* self, size_t size) {
+static size_t libmem_resizenode(struct libmem* self, size_t size) {
 	size_t i;
 	struct libmem_Node* node;
 	
@@ -48,14 +47,22 @@ static void libmem_resizenode(struct libmem* self, size_t size) {
 		self->nodefree = self->nodesize;
 		self->nodesize = size;
 	}
+	return self->nodefree;
 }
 
 static size_t libmem_allocnode(struct libmem* self) {
 	size_t i;
 	if (self->nodefree == INVALID)
 		libmem_resizenode(self, self->nodesize << 1);
+	if (self->nodefree == INVALID)
+		return INVALID;
 	i = self->nodefree;
 	self->nodefree = self->node[i].next;
+	
+	self->node[i].sibling = INVALID;
+	self->node[i].parent = INVALID;
+	self->node[i].prevous = INVALID;
+	self->node[i].next = INVALID;
 	return i;
 }
 
@@ -75,15 +82,18 @@ static size_t libmem_splitnode(struct libmem* self, size_t parent) {
 	
 	i = libmem_allocnode(self);
 	j = libmem_allocnode(self);
-	node = &self->node[parent];
 	if (i == INVALID || j == INVALID)
 		return INVALID;
+		
+	node = &self->node[parent];
+	node->free = 0;
 		
 	self->node[i].parent = parent;
 	self->node[i].sibling = j;
 	self->node[i].level = node->level - 1;
 	self->node[i].segment = node->segment;
 	self->node[i].segment->index = i;
+	self->node[i].free = 0;
 	
 	self->node[j].parent = parent;
 	self->node[j].sibling = i;
@@ -94,6 +104,7 @@ static size_t libmem_splitnode(struct libmem* self, size_t parent) {
 	k = self->free[node->level - self->minlevel - 1];
 	self->node[j].prevous = INVALID;
 	self->node[j].next = k;
+	self->node[j].free = 1;
 	if (k != INVALID)
 		self->node[k].prevous = j;
 	self->free[node->level - self->minlevel - 1] = j;
@@ -110,13 +121,13 @@ static void libmem_combinnode(struct libmem* self, size_t child) {
 	}
 	else
 	{
-		i = self->free[self->node[child].level - self->minlevel];
+		i = self->free[self->node[child].level - self->minlevel - 1];
 		self->node[child].free = 1;
 		self->node[child].prevous = INVALID;
 		self->node[child].next = i;
 		if (i != INVALID)
 			self->node[i].prevous = child;
-		self->free[self->node[child].level - self->minlevel] = child;
+		self->free[self->node[child].level - self->minlevel - 1] = child;
 	}
 }
 
@@ -125,19 +136,20 @@ static void* libmem_allocbylevel(struct libmem* self, size_t level) {
 	size_t i, j;
 	
 	node = NULL;
-	i = level - self->minlevel;
+	i = level - self->minlevel - 1;
 	if (self->free[i] != INVALID)
 	{
 		node = &self->node[self->free[i]];
+		node->free = 0;
 		self->free[i] = node->next;
 	}
-	else for (++i; i < self->maxlevel - self->minlevel; ++i)
+	else for (++i; i < self->maxlevel - self->minlevel - 1; ++i)
 	{
 		if (self->free[i] != INVALID)
 		{
 			j = self->free[i];
 			self->free[i] = self->node[j].next;
-			for (; i > level - self->minlevel; --i)
+			for (; i > level - self->minlevel - 1; --i)
 			{
 				j = libmem_splitnode(self, j);
 				if (j == INVALID) return NULL;
@@ -168,10 +180,7 @@ struct libmem* libmem_new(size_t minlevel, size_t maxlevel) {
 		self->free[i] = INVALID;
 	self->free[i] = libmem_allocnode(self);
 	i = self->free[i];
-	self->node[i].sibling = INVALID;
-	self->node[i].parent = INVALID;
-	self->node[i].prevous = INVALID;
-	self->node[i].next = INVALID;
+	self->node[i].free = 1;
 	
 	self->node[i].segment = (struct libmem_Segment*) self->base;
 	self->node[i].segment->index = i;
@@ -191,25 +200,16 @@ void* libmem_alloc(struct libmem* self, size_t size) {
 	realsize = size + sizeof(struct libmem_Segment);
 	assert(realsize <= (1 << self->maxlevel));
 	
-	if (realsize > (1 << (self->minlevel + 3)))
+	min = self->minlevel;
+	max = self->maxlevel;
+	
+	while (max - min > 1)
 	{
-		min = self->minlevel;
-		max = self->maxlevel;
-		
-		while (max - min > 1)
-		{
-			i = (min + max) >> 1;
-			if (realsize > (1 << i))
-				min = i;
-			else
-				max = i;
-		}
-	}
-	else 
-	{
-		for (i = self->minlevel; i < self->maxlevel; ++i)
-			if (realsize <= (1 << i))
-				break;
+		i = (min + max) >> 1;
+		if (realsize > (1 << i))
+			min = i;
+		else
+			max = i;
 	}
 	return libmem_allocbylevel(self, i);
 }
